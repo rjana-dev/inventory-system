06b9b41c7d9ab74211686e0b456703cac0ba419e
Jananganan <jananganan2002@gmail.com>
2026-07-16 10:05:09 +0530

    Commented someline for explanation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [products, setProducts] = useState([]);
 
+  //This is just a test to check wheather the front end is connected to backend and fetches the data
   useEffect(() => {
     fetch("http://localhost:8888/api/products")
       .then((res) => res.json())
